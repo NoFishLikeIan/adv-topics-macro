2c8277556f9193d76c98eefd6d813e23061004b2
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-01 16:58:51 +0100

    👨‍🎨 better params for ex3

--- src/week-one/main.jl
+++ src/week-one/main.jl
@@ -5,20 +5,22 @@ using Plots
 
 # --- Excercise 3
 
-space = collect(range(0, 1, step=.1))
+low = 0
+up = 2π
+
+space = collect(range(low, up, step=1))
 evaluations = exp.(space)
 
-thickerspace = collect(range(0, 1, step=.01))
+thickerspace = collect(range(low + 0.1, up - 0.1, step=.1))
 
 linear_int = constructlinear(space, evaluations)
 interpolated = linear_int.(thickerspace)
 
+# --- Exercise 4
+size = 100_000_000
 
-# --- Exercise 
-
-size = 100000000
-
-test = vcat(range(0, 1, length=size), range(1, 0, length=50))
+test = vcat(range(0, 100, length=size), range(99, 0, length=50))
 
 @time find_maximum(test, mode="unknown")
 @time find_maximum(test, mode="concave")
+
