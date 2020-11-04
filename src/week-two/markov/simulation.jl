a93654223b75cbbd9821c02bbea970482dc4d54c
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-04 15:50:51 +0100

    🪐 simulation of disc Markov chain using uniform

---
+++ src/week-two/markov/simulation.jl
@@ -0,0 +1,61 @@
+using LinearAlgebra, Distributions, Random
+
+# TODO: Make a Markov struct 
+
+"""
+Construct the psuedo cdf, based on P matrix
+"""
+function pseudoF(P::Matrix{Float64})
+    N, N = size(P)
+    F = copy(P)
+
+    for n in 2:N
+        F[:, n] = F[:, n] + F[:, n - 1]
+    end
+
+    return F
+end
+
+"""
+Constructs the inverse of the pseudo F matrix.
+"""
+function make_inv_F(pseudoF::Matrix{Float64})
+
+    """
+    Takes the partition index of z and a probability value. 
+    Returns the originating z' partition index.
+    """
+    function inv(u::Float64, z_idx::Int)::Int
+        cond_F = pseudoF[z_idx, :]
+
+        for (jdx, prob) in enumerate(cond_F)
+            if u < prob
+                return jdx
+            end
+        end
+
+        return length(cond_F)
+    end
+    
+    return inv
+end
+
+function discrete_sim(
+    P::Matrix{Float64}, partition::Partition, T::Int; 
+    drop::Int=2000)
+
+    if drop > T throw(ErrorException("Variables to drop ($drop) > total ($total)")) end
+
+    inverse_F = make_inv_F(pseudoF(P))
+
+    evolution_jdx = zeros(Int, T)
+    evolution_jdx[1] = rand(1:length(partition))
+
+    for (t, u) in enumerate(rand(Uniform(), T - 1))
+        
+        evolution_jdx[t + 1] = inverse_F(u, evolution_jdx[t])
+    end
+
+    return currygetindex(partition).(evolution_jdx)
+    
+end
