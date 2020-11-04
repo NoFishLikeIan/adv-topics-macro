3666a6b26b9ffaf7d0f92ca6ecc4d119b08e5886
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-04 13:52:43 +0100

    🪐 working on new ar process

--- src/week-two/markov/discretization.jl
+++ src/week-two/markov/discretization.jl
@@ -1,6 +1,7 @@
 using Distributions, Random
 
 include("process.jl")
+include("markov-types.jl")
 
 """
 Discretize a Process based on equidistant points (mode = "equi") or equal probabilities (mode = "imp").
@@ -11,8 +12,8 @@ function tauchen(proc::Process, N::Int, m::Int; mode="equi")
     upper = m * sqrt(var(proc))
     lower = -upper
     
-    partitions = range(lower, upper, length=N)
-    d = (partitions[end] - partitions[end - 1]) / 2
+    partition = Partition(collect(range(lower, upper, length=N)))
+    d = distance(partition)
 
     function scale_f(z::Real) 
         forward = z_p::Real -> F((z + d - proc.evol(z_p)) / var(proc.error))
@@ -24,16 +25,10 @@ function tauchen(proc::Process, N::Int, m::Int; mode="equi")
 
     P = zeros((N, N))
 
-    z_grid = collect(Iterators.product(partitions, partitions))
-
-    P[:, 1] = scale_f(partitions[1])[1].(partitions)
-
-    for i in 2:(N - 1)
-        f, b = scale_f(partitions[i])
-        P[:, i] = f.(partitions) - b.(partitions)
+    for i in 1:N
+        f, b = scale_f(partition[i])
+        P[:, i] = f.(partition) - b.(partition)
     end
-
-    P[:, N] = ones(N) - scale_f(partitions[N])[2].(partitions)
 
     return P
 end
@@ -43,11 +38,10 @@ end
 N = 5
 m = 3
 
-# TODO: Note that this is not the distribution of the excercise, only to practice
 ar = Process(
     Normal(0, σ^2 / (1 - ρ^2)),
     z -> ρ * z,
-    Normal(0, 1)
+    Normal(0, (1 - ρ^2)^2)
 )
 
 P = tauchen(ar, N, m)
