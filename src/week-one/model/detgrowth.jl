b5862b9153e01209ae4f9ffeb559895dfda8a2ca
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-08 17:53:49 +0100

    🪐 stochastic growth

--- src/week-one/model/detgrowth.jl
+++ src/week-one/model/detgrowth.jl
@@ -1,7 +1,17 @@
+include("../../week-two/markov/simulation.jl")
+
 struct DetGrowthModel
     β::Float64
     α::Float64
     z::Float64
+    ε::Float64
+    k_size::Int
+end
+
+struct StochGrowthModel
+    β::Float64
+    α::Float64
+    z::MarkovDiscrete
     ε::Float64
     k_size::Int
 end
