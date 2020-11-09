bae33b77e4ae02083a2e580b2557f77f8acab62a
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-09 11:32:48 +0100

    🧹 🪐 separated stoch and det

---
+++ src/week-two/ogm/stochgrowth.jl
@@ -0,0 +1,27 @@
+include("../markov/simulation.jl")
+include("../markov/process.jl")
+include("../markov/discretization/rouwenhorst.jl")
+
+struct StochGrowthModel
+    β::Float64
+    α::Float64
+    z::MarkovDiscrete
+    f::Function
+    f_prime::Function
+    u_c::Function
+end
+
+function make_z_proc(μ::Float64, σ::Float64, N::Int, ρ::Float64)::MarkovDiscrete
+    μ_y, σ_y = 0., σ / √(1 - ρ^2)
+
+    z = Process(
+        Normal(μ_y, σ_y),
+        ρ,
+        Normal(μ, σ)
+    )
+
+    P, S = rouwenhorst(z, N; numerical=false)
+    markov = MarkovDiscrete(P, S, exp)
+
+    return markov
+end
