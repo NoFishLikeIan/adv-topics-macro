7f99596d7f02bab37fe8523cd24543084a2f0d10
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-09 19:47:05 +0100

    👨‍🎨 plotting of EEE

--- src/week-two/solve-stochgrowth.jl
+++ src/week-two/solve-stochgrowth.jl
@@ -5,7 +5,23 @@ include("ogm/policy_fi.jl")
 include("ogm/value_fi.jl")
 
 
-plot_path = "src/week-two/solutions/plots/"
+plot_path = "src/week-two/solutions/plots/value"
+
+function plot_EEE(EEE_p::Array{Float64}, EEE_v::Array{Float64}, k_space, z_space)
+    n, m = size(EEE_p)
+
+    for h in 1:m
+        z = round(z_space[h], digits=2)
+        plot(xaxis="k", yaxis="EEE(k)", dpi=1000, title="EEE z=$z")
+
+        plot!(k_space, EEE_p[:, h], label="P")
+        plot!(k_space, EEE_v[:, h], label="V")
+        plot!(k_space, abs.(EEE_p[:, h]) - abs.(EEE_v[:, h]), label="|P|-|V|")
+
+        savefig("$plot_path/EEE-$h.png")
+    end
+    
+end
 
 function run_solver(β::Float64, α::Float64, μ::Float64, σ::Float64, ρ::Float64, N::Int; do_plot=false, verbose=false)
 
@@ -23,8 +39,9 @@ function run_solver(β::Float64, α::Float64, μ::Float64, σ::Float64, ρ::Floa
         f, f_prime, u, u_c, u_c
     )
 
-    policy = policy_solve(model; grid_N=grid_N, tol=1e-3, verbose=verbose)
-    V = value_solve(model; grid_N=grid_N, tol=1e-3, verbose=verbose)
+    policy, EEE_p = @time policy_solve(model; grid_N=grid_N, tol=1e-3, verbose=verbose)
+
+    V, EEE_v = @time value_solve(model; grid_N=grid_N, tol=1e-3, verbose=verbose)
 
     if do_plot
         print("Plotting... \n")
@@ -50,16 +67,19 @@ function run_solver(β::Float64, α::Float64, μ::Float64, σ::Float64, ρ::Floa
             plot!(k_x, V[:, h], label="z = $rounded")
         end
         savefig("$plot_path/cross-value-$β-$α-$ρ.png")
+
+        plot_EEE(EEE_p, EEE_v, k_x, support(z))
+
         print("...done!\n")
     end
 
-    return V, policy
+    return V, policy, EEE_p, EEE_v
 end
 
 β, α = .9, .5
 μ, σ, ρ = 0., 1., .7
 N = 3
 
-V, policy = run_solver(
-    β, α, μ, σ, ρ, N; do_plot=true, verbose=true
+run_solver(
+    β, α, μ, σ, ρ, N; do_plot=true, verbose=false
 )
