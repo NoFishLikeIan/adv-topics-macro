b0c4413df68890e3eae923df38226309eec17d28
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-10 22:06:55 +0100

    🧙‍♂️ lazy unit vectors

--- src/comm_utility.jl
+++ src/comm_utility.jl
@@ -29,4 +29,18 @@ function matrix_distance(A::Array{Float64,2}, B::Array{Float64,2})::Float64
     return d
 end
 
-toupper(x, i) = x[1:i - 1] * uppercase(x[i:i]) * x[i + 1:end]
+function toupper(x::String, i::Int)::String
+    x[1:i - 1] * uppercase(x[i:i]) * x[i + 1:end]
+end
+
+# A unit vector
+struct e{T} <: AbstractVector{T}
+    i::Int
+    n::Int
+end
+e(i::Int, n::Int, ::Type{T}=Int) where T = e{T}(i, n)
+function Base.getindex(e::e{T}, i) where T
+    @boundscheck @assert i <= e.n
+    ifelse(i == e.i, one(T), zero(T))
+end
+Base.size(e::e) = (e.n,)
