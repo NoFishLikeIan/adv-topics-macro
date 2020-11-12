fcdc0d258b38977d8a12d4dac81273e486fa064c
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-12 17:48:36 +0100

    🧙‍♂️ multivariate opt perturbation

--- src/week-three/numerical/derivatives.jl
+++ src/week-three/numerical/derivatives.jl
@@ -1,17 +1,26 @@
 # Get the square root of machine epsilon
 sq_ϵ = √eps(Float64)
 
+function h_st(x::Float64)::Float64 
+    max(abs(x), 1.) * sq_ϵ
+end
+function h_st(x::Array{Float64,1})
+    
+    reshape(maximum(hcat(abs.(x), ones(length(x))), dims=2), (length(x),)) * sq_ϵ
+end
+
 function constructtwosided(f::Function)::Function
 
     function f_prime(x::Float64)::Float64
-        h = max(abs(x), 1.) * sq_ϵ
+        h = h_st(x)
         return (f(x + h) - f(x - h)) / 2h
     end
 
     function f_prime(x::Array{Float64,1})::Array{Float64,1}
-        h = max(abs.(x)..., 1)
         n = length(x)
-        return  [ f(x + h * e(i, n)) - f(x - h * e(i, n)) for i in 1:n ] ./ 2h # Is this the best way?
+        h = h_st(x)
+
+        return  [ f(x + h .* e(i, n)) - f(x - h .* e(i, n)) for i in 1:n ] ./ 2h # Is this the best way?
     end
 
     return f_prime
@@ -20,14 +29,15 @@ end
 function constructonesided(f::Function)::Function
 
     function f_prime(x::Float64)::Float64
-        h = max(x, 1.) * sq_ϵ
+        h = h_st(x)
         return (f(x + h) - f(x)) / h
     end
 
     function f_prime(x::Array{Float64,1})::Array{Float64,1}
-        h = max(abs.(x)..., 1)
         n = length(x)
-        return  [ f(x + h * e(i, n)) - f(x) for i in 1:n ] ./ h
+        h = h_st(x)
+        
+        return  [ f(x + h .* e(i, n)) - f(x) for i in 1:n ] ./ h
     end
 
     return f_prime
