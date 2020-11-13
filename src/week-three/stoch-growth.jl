81cef5f36660d45f8b0bd1a04456adeca8190b58
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-13 18:34:45 +0100

    🪐 simply stochastic growth model

--- src/week-three/stoch-growth.jl
+++ src/week-three/stoch-growth.jl
@@ -1,15 +1,35 @@
-using Dolo, Distances
+using Dolo, Distances, Distributions, Random, Plots
 
+Random.seed!(123)
 
 model = yaml_import("src/week-three/models/sgm.yaml")
+plot_path = "src/week-three/solutions/plots"
 
-no_shocks = Dict(:z => [model.calibration.flat[:z]])
+T = 200
 
-T = 101 # Simulation periods
-p_T = 10 # Plotting periods
-shocks = Dict(:z => [fill(0.2, 2); 0.4])
+parameters = model.calibration.flat
 
+ρ, σ = parameters[:rho], parameters[:sigma]
+ϵ = rand(Normal(0, σ), T)
+
+z = zeros(T)
+z[1] = 0.1
+
+for t in 2:T
+    z[t] = z[t - 1] * ρ + ϵ[t]
+end
+
+plot(collect(1:T), z, title="AR(1) prod. simulation", label="z")
+savefig("$plot_path/ar1.png")
+
+shocks = Dict(:z => z)
 
 sol_ref = perfect_foresight(
     model, shocks, 
     T=T, complementarities=false)
+
+k = sol_ref[2, :][1:end - 1]
+
+plot(collect(0:T), k, title="Simulation", label="k(t)")
+
+savefig("$plot_path/k.png")
