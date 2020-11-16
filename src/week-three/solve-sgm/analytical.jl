e8759d0d4710f9f03191cee087aed224d09c9fd8
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-16 10:47:07 +0100

    🪐 nicer analytical policy

--- src/week-three/solve-sgm/analytical.jl
+++ src/week-three/solve-sgm/analytical.jl
@@ -5,15 +5,9 @@ function analytical_policy(model)
     ρ, σ = parameters[:rho], parameters[:sigma]
 
     k_policy(k, y) = α * β * y * k^α
+    c_policy(k, y) = y * k^α - k_policy(k, y)
 
-
-    function c_policy(k, y)
-        k_next = k_policy(k, y)
-
-        return y * k^α - k_policy(k, y)
-    end
-
-    return c_policy
+    return c_policy, k_policy
 end
 
 function analytical_ss(model)
@@ -28,4 +22,18 @@ function analytical_ss(model)
 
     return y, k, c
 
+end
+
+function analytical_eee(model)
+    parameters = model.calibration.flat
+
+    α, β = parameters[:alpha], parameters[:beta]
+    ρ, σ = parameters[:rho], parameters[:sigma]
+
+    c, k_p = analytical_policy(model)
+
+
+    f(k, y) = β * (α * y * k_p(k, y)^(α - 1) ) / c(k_p(k, y), y)
+
+    return (k, y) -> log10(abs(1 - f(k, y)))
 end
