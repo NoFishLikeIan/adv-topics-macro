52fc7aed2b4a0ebd6566d3079d04319b8f9861ad
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-16 10:47:43 +0100

    🧙‍♂️ moved euler errors from python to Julia

---
+++ src/week-three/solve-sgm/perturbation.jl
@@ -0,0 +1,31 @@
+using Dolo, Dolang, StaticArrays
+
+function pert_eee(model; n_steps=1_000, bounds=[.01, 1.])
+    res = time_iteration(model; verbose=false)
+
+    m = model.calibration[:exogenous]
+    x = model.calibration[:controls]
+    p = model.calibration[:parameters]
+    ss = model.calibration[:states]
+
+    f = eval(Dolang.gen_generated_gufun(model.factories[:arbitrage]))
+
+    tab = tabulate(model, res.dr, :k, bounds, ss, m; n_steps=n_steps)
+    c = tab[:c]
+    k_space = tab[:k]
+
+    eee = zeros(size(k_space))
+
+    for (i, k) in enumerate(k_space)
+
+        euler = f(m, [k], x, m, [k], x, p)
+
+        rel_error = 1 - (euler[1] + 1) / c[i]
+
+        eee[i] = log10(abs(rel_error))
+
+    end
+
+    return eee, k_space
+
+end
