8d504cbb5262129d8a7656739ef9402ce8ddc7ec
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-21 18:16:23 +0100

    🧹 unnecessary plotting

--- run-two.jl
+++
@@ -1,8 +0,0 @@
-using Random
-
-Random.seed!(132)
-
-include("init.jl")
-
-include("src/week-two/ar-markov.jl")
-include("src/week-two/solve-stochgrowth.jl")
--- src/week-four/algos/piecewise.jl
+++
@@ -1,9 +0,0 @@
-function compute_invariant(
-    ai::Aiyagari,
-    a_grid::Array{Float64,1}   
-)
-
-    Γ = ai.y.P
-    S = ai.y.S
-
-end
