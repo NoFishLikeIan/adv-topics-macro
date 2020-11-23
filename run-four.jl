2a5e009466fe484b827facee66cd1dfee3a9beae
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-23 09:34:35 +0100

    👨‍🎨 🧹 systematic partial plotting

--- run-four.jl
+++ run-four.jl
@@ -1,3 +1,5 @@
+using Plots
+
 # Environment variables
 const scriptpath = rsplit(@__FILE__, "/", limit=2)[1]
 const do_plot = false
@@ -5,3 +7,61 @@ verbose = get!(ENV, "VERBOSE", "false") == "true"
 plotpath = joinpath(scriptpath, "src/week-four/solutions/plots/")
 
 include("src/week-four/partial.jl")
+
+# --- Running partial equilibrium
+
+
+r = .05
+w = 1.
+
+n_steps = 500
+upperbound = 8.
+
+model = Aiyagari(
+        0.9, 0.1, 7, # AR process parameters ρ, σ, N
+        0, 0.95, 0.33, 0.1, 2 # Model parameters a, β, α, δ, σ_u
+    )
+    
+
+verbose && print("Finding policy function...\n")
+a′, a_grid = policysolve(
+    model, r, w; 
+    n_steps=n_steps, upperbound=upperbound, 
+    verbose=verbose
+)
+
+# Plot the policy function
+plot(title="Policy", xaxis="a")
+
+for y in model.y.transformation.(model.y.S)
+    plot!(
+            a_grid,
+            (a -> a′(a, y)).(a_grid),
+            label="a′(a | y = $(@sprintf("%.2f", y)))",
+            xaxis="a"
+        )
+end
+
+append_filename = r != .05 ? "_$(Int(r * 100))" : ""
+
+savefig("src/week-four/solutions/plots/partial/policy$append_filename.png")
+
+
+verbose && print("Finding steady state distribution...\n")
+
+λ_mc = distribution_pdf(a′, a_grid, model; mc=true, verbose=verbose)
+λ_eig =  distribution_pdf(a′, a_grid, model; mc=false, verbose=verbose)
+
+mc_ys = λ_mc.(a_grid) / sum(λ_mc.(a_grid))
+
+plot(
+    a_grid, mc_ys, color=:red, label="MC", alpha=0.5,
+    title="Stable distribution", xaxis="a", yaxis="λ(a)"
+)
+
+eig_ys = λ_eig.(a_grid) / sum(λ_eig.(a_grid))
+
+plot!(a_grid, λ_eig, color=:black, label="Eig", alpha=0.5)
+
+
+savefig("src/week-four/solutions/plots/partial/stat_dist$append_filename.png")
--- src/week-four/partial.jl
+++ src/week-four/partial.jl
@@ -11,17 +11,13 @@ include("../commons/matrix.jl")
 
 include("../week-two/markov/discretization/tauchen.jl")
 
-plot_dens = true
-
 using Plots
 
-function solvepartial(
-    model::Aiyagari, r::Float64, w::Float64;
-    mc=true, grid_N=1_000, end_grid=false,
-    verbose=false, cache=false, kwargs...)
-
-    a′, a_grid = policysolve(model, r, w; n_steps=grid_N, upperbound=10., verbose=verbose, kwargs...)
- 
+"""
+Construct the wealth distribution pdf based on the MonteCarlo 
+or Eigenvector method
+"""
+function distribution_pdf(a′, a_grid, model; mc=false, kwargs...)
 
     if mc
         kde = distribution_mc(a′, a_grid, model; 
@@ -41,39 +37,19 @@ function solvepartial(
 
     end
 
-    
-    return λ, a′, a_grid 
+    return λ
+
 end
 
-if plot_dens
+function solvepartial(
+    model::Aiyagari, r::Float64, w::Float64;
+    mc=true, n_steps=1_000, end_grid=false,
+    verbose=false, cache=false, kwargs...)
 
-    r = .9
-    w = 1.
+    a′, a_grid = policysolve(model, r, w; n_steps=n_steps, upperbound=10., verbose=verbose, kwargs...)
+
+    λ = distribution_pdf(a′, a_grid, model; mc=mc, verbose=verbose, kwargs...)
+
     
-    model = Aiyagari(
-        0.9, 0.1, 7, # AR process parameters ρ, σ, N
-        0, 0.95, 0.33, 0.1, 2 # Model parameters a, β, α, δ, σ_u
-    )
-    
-    λ, a′, a_grid = solvepartial(
-        model, r, w, grid_N=100;
-        verbose=true, mc=false, end_grid=true)
-
-    plot(title="Policy", xaxis="a")
-
-    for y in model.y.transformation.(model.y.S)
-        plot!(
-            a_grid,
-            (a -> a′(a, y)).(a_grid),
-            label="a′(a | y = $(@sprintf("%.2f", y)))",
-            xaxis="a"
-        )
-    end
-
-    savefig("src/week-four/solutions/plots/policy_$(Int(r * 100)).png")
-
-
-    plot(a_grid, λ, title="Stable distribution", xaxis="a", color=:red)
-
-    savefig("src/week-four/solutions/plots/stat_dist_$(Int(r * 100)).png")
+    return λ, a′, a_grid 
 end
