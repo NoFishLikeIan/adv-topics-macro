cae8e000fd4536047c9798f20e38263f31b4533a
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-26 17:05:45 +0100

    🧹 calibration

--- run-five.jl
+++ run-five.jl
@@ -3,40 +3,41 @@ include("src/commons/matrix.jl")
 
 include("src/week-five/markov/state.jl")
 include("src/week-five/denhaan.jl")
-include("src/week-five/policy/pfi.jl")
+include("src/week-five/policy/endgrid.jl")
 include("src/week-five/krusell-smith/main.jl")
 
 
 using Plots
 
-default(size=(12, 8), dpi=600)
+default(dpi=600)
 
-do_policy = true
+do_policy = false
 plot_path = "src/week-five/solutions/plots/"
 model = DenHaanModel()
 
 # --- Test policy function 
 if do_policy
-    N = 10
+    N_a = 100
+    N_m = 10
     
     ho_Ψ(b0, b1) = (z, K) -> exp(b0 + b1 * log(K))
     Ψ = ho_Ψ(0., 1.)
     
-    policy = pfi(
+    policy = endgrid_method(
         makeproduction(model),
         makeutility(model),
-        Ψ, model, (N, N);
-        ρ=0.7)
+        grid_bounds=[.01, 20.],
+        Ψ, model, (N_a, N_m);
+        ρ=0.5, verbose=true)
 
     fix_m = 1.
-    as = range(0.01, 10., length=100)
-    plot(
-        title="Policy function", legend=:left,
-        xlabel="a", ylabel="a′(a)"
-    )
+    as = range(0.01, 20., length=100)
 
-    for (z_f, ϵ_f) in mode.ζ.S
-        ys_pol = policy.(as, fix_m, z_f, ϵ_f)
+    plot(title="Policy function", legend=:left,
+        xlabel="a", ylabel="a′(a)")
+
+    for (z_f, ϵ_f) in model.ζ.S
+        ys_pol = policy.(as, fix_m, z_f, collect(Float64, ϵ_f))
         plot!(
             as, ys_pol,
             label="a′(a | $z_f, $ϵ_f)"
@@ -45,3 +46,4 @@ if do_policy
 
     savefig("$plot_path/policy.png")
 end
+
