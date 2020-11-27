4e4d0c22a3383985bd673f9133b5a775223a5d10
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-27 11:06:56 +0100

    🎉 conditional markov simulation

--- src/week-five/markov/state.jl
+++ src/week-five/markov/state.jl
@@ -1,4 +1,7 @@
-using Distributions
+include("../../week-two/markov/simulation.jl")
+
+using Distributions, Parameters
+using Base.Threads
 
 State = Union{Any,Tuple{Vararg{Any}}} # make variable type
 
@@ -7,13 +10,12 @@ struct StateMarkov
     P::Matrix{Float64}
 end
 
-function markovstate_simulation(
+function simulation(
     markov::StateMarkov, T::Int; 
-    drop::Int=0, s0::State=nothing
-)::Vector{State}
+    drop::Int=0, s0::State=nothing)::Vector{State}
 
     if drop > T throw("Variables to drop ($drop) > total ($total)") end
-    if s0 ∉ markov.S throw("Initial state not in state space") end
+    if !isnothing(s0) && s0 ∉ markov.S throw("Initial state not in state space") end
     F_inv = make_inv_F(pseudoF(markov.P))
     
     evolution_jdx = zeros(Int, T)
@@ -29,4 +31,46 @@ function markovstate_simulation(
     sim = [markov.S[j] for j in evolution_jdx[start:end]]
 
     return sim
+end
+
+"""
+Simulate conditionally on an aggregate vector of shock
+"""
+function conditional_simulation(
+    jointmarkov::StateMarkov, aggshock::Vector{State}, N::Int)::Matrix{Float64}
+
+    @unpack S, P = jointmarkov
+
+    S_z = unique([st[1] for st in S])
+
+    T = length(aggshock)
+
+    # Construct an array of π(z′ | ϵ′, z, ϵ) conditional transition matrices
+    rzeros = findall(st -> st[2] == 0, S) 
+    rones = findall(st -> st[2] == 1, S)
+
+    Ns = length(rzeros)
+    shock_Ps = Array{Float64}(undef, T - 1, Ns, Ns)
+
+    # TODO: Check the order of the simulation
+    @threads for t in 2:T
+        from = aggshock[t - 1] == 1 ? rones : rzeros
+        to = aggshock[t] == 1 ? rones : rzeros
+
+        shock_Ps[t - 1, :, :] = rownormal(P[from, to])
+    end
+
+    # Simulate a process for z, long N
+    sim_jdx = Array{Int}(undef, T, N)
+    sim_jdx[1, :] = rand(1:Ns, N)
+
+    for (t, sampled_u) in enumerate(eachrow(rand(Uniform(), T - 1, N)))
+        thisP = shock_Ps[t, :, :]
+        F_inv = make_inv_F(pseudoF(thisP))
+
+        sim_jdx[t + 1, :] = @. F_inv(sampled_u, sim_jdx[t, :])
+    end
+
+    sim = (j -> S_z[j]).(sim_jdx)
+    
 end
