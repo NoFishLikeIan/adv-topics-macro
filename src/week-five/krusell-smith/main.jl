c08fa6709757fd6bc4284504db73ea5f096b10bf
NoFishLikeIan <andreatitton96@gmail.com>
2020-11-28 19:21:01 +0100

    🚀 Krussel-Smith full algorithm

--- src/week-five/krusell-smith/main.jl
+++ src/week-five/krusell-smith/main.jl
@@ -6,26 +6,65 @@ for a Den Haan et al. model.
 """
 function krusellsmith(
     model::DenHaanModel;
-    b0=[1., 1.],
+    B0=[0., 0., 0., 1.],
     ρ=0.7, N_a=100, N_m=10,
-    ϵ_m=1e-3, ϵ_g=1e-3,
+    ϵ_m=1e-3, max_iter=1_000,
     kwargs...
 )
     u, u′, inv_u′ = makeutility(model)
     R, w, τ = makeproduction(model)
 
-    ho_Ψ(b0, b1) = (z, K) -> exp(b0 + b1 * log(K)) # FIXME: How is this depending on z?
-    Ψ = ho_Ψ(b0...) # Initial guess for forecasting rule Ψ
+    function ho_Ψ(B)
+        function Ψ(z, K)
+            boom = z == 1.01 ? 1. : 0.
 
-    policy = endgrid_method(
-        makeproduction(model),
-        makeutility(model),
-        Ψ, model, (N_a, N_m);
-        grid_bounds=[0.01, 20.],
-        ρ=ρ, kwargs...)
+            return exp(B' * [1., boom, boom * log(K), log(K)])
+        end
 
+        return Ψ
+    end
 
-    simulated_a = economysim(policy, model; kwargs...)
+    B = B0 # Initial guess for forecasting rule Ψ
 
+    X = Array{Float64}(undef, T - 1, 4) # regressors
+
+    for iter in 1:max_iter
+        Ψ = ho_Ψ(B)
+
+        verbose && print("Computing policy...\n")
+        policy = endgrid_method(
+            Ψ, model, (N_a, N_m);
+            tol=3e-1,
+            grid_bounds=[0.01, 10.],
+            ρ=ρ, kwargs...)
+
+        verbose && print("simulating economy...\n")
+        as, zs = economysim(policy, model; kwargs...)
+        ms = log.(mean(as, dims=2))
+
+        X[:, 1] .= 1.
+        X[:, 2] = [z == 1.01 ? 1. : 0. for z in zs[1:end - 1]]
+        X[:, 3] = ms[1:end - 1] .* X[:, 2]
+        X[:, 4] = ms[1:end - 1]
+
+        y = ms[2:end]
+
+        B′ = inv(X' * X) * X'y
+        verbose && print("OLSing, new coefficients $(B′)...\n")
+
+        d = B′ - B
+
+        err_distance = maximum(abs.(d))
+
+        verbose && print("Ψ iteration: $iter / $max_iter: $(@sprintf("%.4f", err_distance))...\n")
+
+        if err_distance < ϵ_m 
+            verbose && print("Found policy in $iter iterations (|x - x'| = $(@sprintf("%.4f", err_distance))\n")
+            return policy
+        end
+
+        B = B′
+
+    end
 end
 
